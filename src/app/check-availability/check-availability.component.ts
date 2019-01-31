de0d00f504647e4a032449e50c4ceb338d27c757
Sahil Pahuja <isahilpahuja@gmail.com>
2019-01-31 22:25:25 +0530

    final code

--- src/app/check-availability/check-availability.component.ts
+++ src/app/check-availability/check-availability.component.ts
@@ -22,17 +22,31 @@ export class CheckAvailabilityComponent implements OnInit {
     if (!checkForm.form.valid) {
       alert('All Fields Are Mandatory');
     } else {
-      alert('Message Sent!');
+      const httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+      };
+      const request = {
+        user: this.model
+      };
+      this.http.post('http://localhost:8080/user/add', this.model)
+        .pipe(map((response: any) => response)).subscribe(
+          (data: any) => {
+            alert('Message Sent!');
+          },
+          err => {
+            alert('Oops!! Something Went Wrong!!');
+          }
+        );
       this.router.navigate(['./used-car-portal']);
     }
   }
 
   reset() {
     this.model = {
-      firstname: '',
-      lastname: '',
+      firstName: '',
+      lastName: '',
       email: '',
-      query: '',
+      notes: '',
       agree: ''
     };
   }
